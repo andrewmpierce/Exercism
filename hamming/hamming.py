5ab44eb547184fb0299ebedbcb7e4ee267bd909c
apierce1992 <apierce1992@gmail.com>
2015-09-07 22:02:38 -0400

    Used enumerate method

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,6 +1,13 @@
+# def distance(string, string2):
+#     hamming = 0
+#     for a, b in zip(string, string2):
+#             if a != b:
+#                 hamming +=1
+#     return hamming
+
 def distance(string, string2):
     hamming = 0
-    for a, b in zip(string, string2):
-            if a != b:
-                hamming +=1
+    for idx, nucleotide in enumerate(string):
+        if string[idx] != string2[idx]:
+            hamming += 1
     return hamming
